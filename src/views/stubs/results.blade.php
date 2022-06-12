b4ee6f51c040ebbba7eded791851646d129d594a
Paul Coull <grafxflow@users.noreply.github.com>
2022-06-12 23:09:54 +0100

    Update results.blade.php

--- src/views/stubs/results.blade.php
+++ src/views/stubs/results.blade.php
@@ -19,6 +19,10 @@
 
 <h2 class="text-xl font-semibold">Poll: {{ $question }}</h2>
 
+<div class="w-full bg-gray-200 rounded-full">
+    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style="width: 25%"> 25%</div>
+</div>
+
 <div class="w-full bg-gray-200 h-1 mb-6">
     <div class="bg-blue-600 h-1" style="width: 0%"></div>
 </div>
@@ -37,7 +41,7 @@
 
 @foreach($options as $option)
     <div class="result-option-id">
-        <strong>{{ $option->name }}</strong><span class='pull-right'>{{ $option->percent }}%</span>
+        <strong>{{ $option->name }}</strong><span class="pull-right">{{ $option->percent }}%</span>
         <div class="progress">
             <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="{{ $option->percent }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $option->percent }}%">
                 <span class="sr-only">{{ $option->percent }}% Complete</span>
