a8b8f8cb23714a16d74968c7601534b0e5c020e0
Paul Coull <grafxflow@users.noreply.github.com>
2022-06-12 23:36:39 +0100

    Update results.blade.php

--- src/views/stubs/results.blade.php
+++ src/views/stubs/results.blade.php
@@ -17,7 +17,14 @@
     </div>
 @endif
 
-<h2 class="text-xl font-semibold pb-3">Poll: {{ $question }}</h2>
+<div class="flex flex-col">
+    <div class="h-1 bg-gray-200 rounded overflow-hidden">
+        <div class="w-24 h-full bg-nubnewsred-800"></div>
+    </div>
+    <div class="flex flex-wrap sm:flex-row flex-col pt-2 pb-1">
+        <h1 class="flex-1 text-gray-900 font-medium title-font text-2xl mb-0 sm:mb-0">Poll: <span class="text-nubnewsred-800">{{ $question }}</span></h1>
+    </div>
+</div>
 
 @foreach($options as $option)
     <div class="w-full pb-3">
