928a7f6320d4d6e909a6a374fd7e3fd68c02400c
Paul Coull <grafxflow@users.noreply.github.com>
2022-06-12 23:45:12 +0100

    Update radio.blade.php

--- src/views/stubs/radio.blade.php
+++ src/views/stubs/radio.blade.php
@@ -1,12 +1,14 @@
 <form method="POST" action="{{ route('poll.vote', $id) }}" >
     @csrf
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                <span class="glyphicon glyphicon-arrow-right"></span> {{ $question }}
-            </h3>
+    <div class="flex flex-col">
+        <div class="h-1 bg-gray-200 rounded overflow-hidden">
+            <div class="w-24 h-full bg-nubnewsred-800"></div>
+        </div>
+        <div class="flex flex-wrap sm:flex-row flex-col pt-2 pb-1">
+            <h1 class="flex-1 text-gray-900 font-medium title-font text-2xl mb-0 sm:mb-0">Poll: <span class="text-nubnewsred-800">{{ $question }}</span></h1>
         </div>
     </div>
+    
     <div class="panel-body">
         <ul class="list-group">
             <input value="{{ request()->userEmail }}" type="hidden" name="newsletter_id">
@@ -22,7 +24,8 @@
             @endforeach
         </ul>
     </div>
-    <div class="panel-footer">
-        <input type="submit" class="btn btn-primary btn-sm" value="Vote" />
+
+    <div>
+        <input type="submit" class="w-full px-4 py-2 font-medium text-center text-white transition-colors duration-200 rounded-md bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-1 dark:focus:ring-offset-darker" value="Vote">
     </div>
 </form>
